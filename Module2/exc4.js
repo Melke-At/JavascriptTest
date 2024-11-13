57dd7657bc07fb39057eb65f3d3a02e47974089e
Melkamu <melkemech@gmail.com>
2024-11-13 10:51:49 +0200

    Module2

---
+++ Module2/exc4.js
@@ -0,0 +1,13 @@
+'use strict';
+const numbers = [];
+let num;
+while(true){
+const num = parseFloat(prompt ("Enter number: or zero to terminate"));
+if (num === 0){
+  break;
+}
+numbers.push(num);
+
+}
+numbers.sort((a, b) => b - a);
+console.log(numbers);
