2be81f2ff3992790b668dad4feb6b9b36a993fda
Melkamu <melkemech@gmail.com>
2024-11-29 13:21:49 +0200

    exc9

--- Module1/exc9.js
+++ Module1/exc9.js
@@ -1,37 +1,23 @@
 
-  'use strict'
-  let num = parseInt(prompt("Please enter your number: "))
-    if (num === 0 ||num === 1 ){
-       document.querySelector('#target').innerHTML = `${num} is not a prime number.`;
-    }
-    else if (num ===2){
-      document.querySelector('#target').innerHTML = `${num} is a prime number.`;
-    }
-    else if (num < 0) {
-      document.querySelector('#target').innerHTML = `Please enter a non-negative integer.`;
-    }
-      else if (isNaN(num))  {
-      document.querySelector('#target').innerHTML = `Please enter a valid integer.`;
+'use strict'
+let num = prompt('Enter an integer')
+  if(num <= 1){
+    document.querySelector('#target').innerHTML = `The number ${num} is not a prime number`
 
-    }
-    else {
-      let isPrime = true;
-      if(num % 2 ===0){
-        isPrime = false;
-      }
-      else {
-        for (let i = 3; i <= Math.sqrt(num); i + 2) {
-          if (num % i === 0) {
-            isPrime = false;
-            break;
-          }
-        }
-      }
-      if(isPrime) {
-          document.querySelector('#target').innerHTML = `${num} is a prime number.`
-        }
-      else {
-        document.querySelector('#target').innerHTML = `${num} is not a prime number.`
+  }
+  let divisorCount = 0;
 
-      }
+  for (let i = 1; i <= num; i++){
+    if (num % i === 0){
+      divisorCount++
     }
+  }
+  if (divisorCount === 2){
+        document.querySelector('#target').innerHTML = `The number ${num} is a prime number`
+
+  }
+  else {
+        document.querySelector('#target').innerHTML = `The number ${num} is not a prime number`
+
+
+}
